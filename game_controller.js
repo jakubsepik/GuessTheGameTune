7fb1d28845c4262ad9c30dc66888ecdfeb43a16d
Jakub Šepeľa <jakubsepik@gmail.com>
2025-01-15 23:00:35 +0100

    version 1.0

--- game_controller.js
+++ game_controller.js
@@ -1,41 +1,87 @@
-const {
-  getNextAudioFile,
-  getPlayer,
-  playSong,
-  stopSong,
-  getLength,
-} = require("./audio_controller.js");
+const { EmbedBuilder } = require('discord.js');
+const AC = require("./audio_controller.js");
 
 class GameController {
-  constructor() {
-    this.state = "idle"; // Object to store the state of the game
+  constructor(maxSecs) {
+    this.state = "idle"; // idle, playing
     this.rounds = 0; // Variable to keep track of the number of rounds
     this.songName = ""; // Variable to store the name of the song
-    this.secs = 25;
+    this.nameGuess = []; // Variable to store the users correct guess
+    this.maxSecs = maxSecs;
+    this.secs = maxSecs;
+    this.hints = 0;
     this.channel = null;
     this.points = {};
+    this.songNameMessage = null;
+  }
+
+  sendPoints(){
+    const fields = Object.entries(this.points).map(([player, points]) => {
+      return { name: player, value: points.toString() };
+    });
+    const embed = new EmbedBuilder()
+    .setTitle('Points')
+    .addFields(fields)
+    .setColor('#0099ff');
+
+  this.channel.send({ embeds: [embed] });
+  }
+
+  #createEmbed(content){
+    return new EmbedBuilder()
+        .setColor('#0099ff')
+        .setDescription(content);
+  }
+
+  async #sendSongGuessMsg(){
+    const tmp = "**" + this.nameGuess.join('** **') + "** | ***"+this.secs+ "***";
+    if(this.songNameMessage){
+      await this.songNameMessage.edit({embeds: [this.#createEmbed(tmp)]});
+    }else{
+      this.songNameMessage = await this.channel.send({embeds: [this.#createEmbed(tmp)]});
+    }
+  }
+
+  #hint(){
+    if(this.maxSecs/2<this.secs || this.hint>=this.nameGuess.length/2)return
+    if(Math.random()<((this.maxSecs - this.secs) / this.maxSecs)){
+      let index = Math.floor(Math.random() * this.nameGuess.length);
+      while(this.nameGuess[index]!=='_'){
+        index = Math.floor(Math.random() * this.nameGuess.length);
+      }
+      this.nameGuess[index] = this.songName[index];
+      this.hints++;
+    }
+    this.#sendSongGuessMsg();
   }
 
   audioInit() {
-    return getPlayer();
+    return AC.getPlayer();
   }
 
-  // Method to start a new game
-  playSong() {
+  async playSong() {
     if (this.state !== "idle") return;
     this.rounds++;
+    this.songNameMessage = null;
+    this.hints = 0;
+
+    this.channel.send({embeds: [this.#createEmbed("Starting round "+this.rounds)]});
     console.log(
-      "Song number " + this.rounds + " out of " + getLength() + " is playing!"
+      "Song number " +
+        this.rounds +
+        " out of " +
+        AC.getLength() +
+        " is playing!"
     );
-    const audio = getNextAudioFile();
+
+    const audio = AC.getNextAudioFile();
     this.songName = audio.name;
+    this.nameGuess = audio.name.split("").map(() => '_');
+    await this.#sendSongGuessMsg();
     console.log("Song name: " + this.songName);
-    playSong(audio.path);
+    AC.playSong(audio.path);
     this.state = "playing";
 
-    setTimeout(() => {
-      stopSong();
-    }, 15_000);
 
     this.timer();
   }
@@ -44,33 +90,36 @@ class GameController {
   checkGuess(message) {
     const { content, author } = message;
     if (this.state === "idle") return;
-    if (content.toLowerCase() === this.songName.toLowerCase()) {
-      if (!this.points[author.id]) {
-        this.points[author.id] = 1;
-      } else {
-        this.points[author.id]++;
+    if (content.trim().toLowerCase().replace(/[^a-z0-9]/gi, '') === this.songName.toLowerCase()) {
+      if (!this.points[author.displayName]) {
+        this.points[author.displayName] = 0;
       }
-      this.state = "idle";
-      this.secs = 25;
-      //message.delete();
-      message.channel.send(message.author.username + " got it right!");
+      this.points[author.displayName]+=this.secs;
+      //message.channel.send(author.displayName + " got it right!");
+      author.send("You got it right! You gain "+this.secs+" points!");
     }
+    message.delete();
   }
 
-  timer() {
+  stopRound(){
+    this.secs = this.maxSecs;
+    AC.stopSong();
+    this.state = "idle";
+  }
+
+  async timer() {
     if (this.state === "idle") return;
-    if (this.secs === 10) {
-      this.channel.send("**10 seconds left!**");
-    } else if (this.secs < 10) {
-      this.channel.send("**" + this.secs + "**");
-    }
-    this.secs--;
+    await this.#sendSongGuessMsg();
     if (this.secs <= 0) {
-      this.secs = 25;
-      this.state = "idle";
-      this.channel.send("Time's up! The game was ***" + this.songName + "***");
-      return;
-    } else return new Promise(() => setTimeout(() => this.timer(), 1000));
+      AC.stopSong();
+      this.channel.send({embeds: [this.#createEmbed("Time's up! The game was ***" + this.songName + "***")]});
+      this.stopRound();
+    } else
+      setTimeout(() => {
+        this.secs--;
+        this.#sendSongGuessMsg();
+        this.timer();
+      }, 1000);
   }
 }
 
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 const { joinVoiceChannel } = require("@discordjs/voice");
 
 const GameController = require("./game_controller.js");
-var game = new GameController();
+var game = new GameController(15);
 const player = game.audioInit();
 var connection, subscription;
 
@@ -24,55 +24,50 @@ client.on("ready", () => {
 client.on(Events.MessageCreate, (message) => {
   if (message.author.bot) return;
 
-  switch (message.content) {
-    case "start":
-      if(!message.member.hasPermission("ADMINISTRATOR")) return
-      const channel = client.guilds.cache
-        .get(message.guildId)
-        .members.cache.get(message.author.id).voice.channel;
+  if (message.member.permissions.has("ADMINISTRATOR"))
+    switch (message.content) {
+      case "start":
+        const channel = client.guilds.cache
+          .get(message.guildId)
+          .members.cache.get(message.author.id).voice.channel;
 
-      if (!channel)
-        return message.reply("You need to join a voice channel first!");
-      game.channel = message.channel;
-      connection = joinVoiceChannel({
-        channelId: channel.id,
-        guildId: message.guildId,
-        adapterCreator: channel.guild.voiceAdapterCreator,
-      });
-      subscription = connection.subscribe(player);
-      break;
+        if (!channel)
+          return message.reply("You need to join a voice channel first!");
+        game.channel = message.channel;
+        connection = joinVoiceChannel({
+          channelId: channel.id,
+          guildId: message.guildId,
+          adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+        subscription = connection.subscribe(player);
+        break;
 
-    case "play":
-      if(!message.member.hasPermission("ADMINISTRATOR")) return
-      if (!connection) return;
-      game.playSong();
-      break;
+      case "play":
+        if (!connection) return;
+        game.playSong();
+        break;
 
-    case "stop":
-      if(!message.member.hasPermission("ADMINISTRATOR")) return
-      if (connection && connection._state.status !== "destroyed")
-        connection.destroy();
-      if (subscription) subscription.unsubscribe();
-      connection = null;
-      break;
+      case "stop":
+        if(game.state == "playing"){
+          game.stopRound();
+          return;
+        } 
+        if (connection && connection._state.status !== "destroyed")
+          connection.destroy();
+        if (subscription) subscription.unsubscribe();
+        connection = null;
+        break;
 
-    case "points":
-      if(!message.member.hasPermission("ADMINISTRATOR")) return
-      const userScores = Object.entries(game.points);
-      userScores.forEach(([userId, score]) => {
-        const user = client.users.cache.get(userId);
-        if (user) {
-          message.channel.send(`${user.username}: ${score}`);
-        }
-      });
-      break;
+      case "points":
+        game.sendPoints();
+        break;
 
-    default:
-      if (!connection) return;
-      game.checkGuess(message);
+      default:
+        if (game.state == "idle") return;
+        game.checkGuess(message);
 
-      break;
-  }
+        break;
+    }
 });
 
 client.login(process.env.TOKEN);
